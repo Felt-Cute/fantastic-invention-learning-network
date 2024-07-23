9d802dea105a618f96a5e8067f3aac3804eb63d1
Devin Catuns <devincatuns1@gmail.com>
2024-07-23 14:01:07 -0400

    init project

---
+++ src/main/java/com/dcat23/learningnetwork/FantasticInventionLearningNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.dcat23.learningnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasticInventionLearningNetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasticInventionLearningNetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dcat23/learningnetwork/FantasticInventionLearningNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcat23.learningnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasticInventionLearningNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
