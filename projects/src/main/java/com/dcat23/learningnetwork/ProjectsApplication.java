07c7e32b7ae21af42cce54416bc3e0a847af9d0e
Devin Catuns <devincatuns1@gmail.com>
2024-07-25 17:21:00 -0400

    feat(projects): fetch Jwt token from UserServiceClient

--- projects/src/main/java/com/dcat23/learningnetwork/ProjectsApplication.java
+++ projects/src/main/java/com/dcat23/learningnetwork/ProjectsApplication.java
@@ -1,13 +1,18 @@
 package com.dcat23.learningnetwork;
 
+import com.dcat23.learningnetwork.dto.LoginRequest;
+import com.dcat23.learningnetwork.security.JwtToken;
+import com.dcat23.learningnetwork.service.AuthService;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @OpenAPIDefinition(
     info = @Info(
@@ -33,5 +38,18 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class ProjectsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProjectsApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(AuthService authService) {
+        return args -> {
+            LoginRequest loginRequest = new LoginRequest("dcat@filn.com", "thisisabadpassword");
+            authService.authenticate(loginRequest);
+        };
+    }
+
+    @Bean
+    public JwtToken jwtToken() {
+        return new JwtToken();
     }
 }
---
+++ projects/src/main/java/com/dcat23/learningnetwork/service/AuthService.java
@@ -0,0 +1,24 @@
+package com.dcat23.learningnetwork.service;
+
+import com.dcat23.learningnetwork.dto.LoginRequest;
+import com.dcat23.learningnetwork.dto.LoginResponse;
+import com.dcat23.learningnetwork.security.JwtToken;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class AuthService {
+
+    private final UserServiceClient userService;
+    private final JwtToken jwtToken;
+
+    public void authenticate(LoginRequest loginRequest) {
+        LoginResponse jwtResponse = userService.login(loginRequest);
+        jwtToken.setToken(jwtResponse.accessToken());
+        log.debug("Authentication successful");
+    }
+}
