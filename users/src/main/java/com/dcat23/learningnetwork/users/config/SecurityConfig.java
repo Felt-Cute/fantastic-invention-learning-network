9489f3cc3803022eba22045620823769125c7eb6
Devin Catuns <devincatuns1@gmail.com>
2024-07-24 23:57:55 -0400

    feat: UserServiceImpl.loginUser added

--- users/src/main/java/com/dcat23/learningnetwork/users/config/SecurityConfig.java
+++ users/src/main/java/com/dcat23/learningnetwork/users/config/SecurityConfig.java
@@ -5,13 +5,16 @@ import com.dcat23.learningnetwork.users.exception.GlobalAuthenticationEntryPoint
 import com.dcat23.learningnetwork.users.filter.JwtTokenGeneratorFilter;
 import com.dcat23.learningnetwork.users.filter.JwtTokenValidatorFilter;
 import com.dcat23.learningnetwork.users.security.CorsConfigurationSourceImpl;
+import com.dcat23.learningnetwork.users.security.UsernamePwdAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -53,8 +56,14 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
-        return authConfig.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService) throws Exception
+    {
+        UsernamePwdAuthenticationProvider authProvider =
+                new UsernamePwdAuthenticationProvider(userDetailsService);
+        ProviderManager providerManager = new ProviderManager(authProvider);
+        providerManager.setEraseCredentialsAfterAuthentication(false);
+        return providerManager;
     }
 
     @Bean
